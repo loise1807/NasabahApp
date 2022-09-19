2660f5d96bfc29601946bee91dd721e9e57636a4
Loise Michael Lumban Raja <mickhaelloise1406@gmail.com>
2022-09-19 22:07:20 +0700

    Nasabah Database

---
+++ nasabah.sql
@@ -0,0 +1,13 @@
+-- Database: dbnasabah
+
+-- DROP DATABASE IF EXISTS dbnasabah;
+
+CREATE DATABASE dbnasabah
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Indonesia.1252'
+    LC_CTYPE = 'English_Indonesia.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
